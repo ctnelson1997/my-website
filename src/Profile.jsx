4c2f8351ac8b141da5895d864d1d1d81db372536
Cole Nelson <ctnelson1997@gmail.com>
2024-06-04 16:19:16 -0500

    depl

--- src/Profile.jsx
+++
@@ -1,8 +0,0 @@
-import { Card } from "react-bootstrap";
-
-export default function Profile() {
-    return <Card style={{textAlign: "center", margin: "1rem", padding: "0.5rem"}}>
-    <h1>Welcome!</h1>
-    <p>My name is Cole.</p>
-  </Card>
-}
